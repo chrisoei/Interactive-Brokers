41bcf7724deb14f8a6e52bb8cc3fce4fa092b04d
btowner01 <btowner01@gmail.com>
2015-04-17 01:25:39 -0400

    Hour11 package with User-Interface based on SWING

---
+++ src/com/bti3global/hour11/Hour11.java
@@ -0,0 +1,33 @@
+package com.bti3global.hour11;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+public class Hour11 {
+
+	public static void main(String[] args) {
+        DataCollector analytics_data = new DataCollector();
+        analytics_data.setVisible(true);
+    }
+
+    static public void inform( final Component parent, final String str) {
+    if( SwingUtilities.isEventDispatchThread() ) {
+    	showMsg( parent, str, JOptionPane.INFORMATION_MESSAGE);
+    }
+    else {
+        SwingUtilities.invokeLater( new Runnable() {
+			public void run() {
+				showMsg( parent, str, JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+    }
+}
+
+static private void showMsg( Component parent, String str, int type) {    	
+    // this function pops up a dlg box displaying a message
+    JOptionPane.showMessageDialog( parent, str, "Hour 11 Analytics - IB", type);
+}
+
+}
+
